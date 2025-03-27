ae3e8304012c7222a731f1ecb7cd6765282479d8
Evan Ewart <evanjewart4234@gmail.com>
2025-03-27 13:35:12 -0700

    Added homepage nav bar and visuals

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+const app = express();
+        app.get("/products",(req, res) => { });
+
+dotenv.config();
+console.log(process.env.MONGO_URI);
+
+app.listen(5000, () => {
+    console.log("Server started at http://localhost:5000");
+    connectDB();
+});
