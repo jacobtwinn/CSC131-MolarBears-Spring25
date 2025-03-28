02b9934b6438abe8bcd94d04ac2f702743c87604
tinowidrin <twidrin@csus.edu>
2025-03-27 21:33:22 -0700

    First commit

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+dotenv.config();
+const app = express();
+app.get("/products",(req, res) => { });
+app.listen(5000, () => {
+    console.log("Server started at http://localhost:5000");
+    connectDB();
+});
