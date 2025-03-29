0577f8368e8fc52b720d40d7abc28e2e11743076
tinowidrin <twidrin@csus.edu>
2025-03-28 19:09:02 -0700

    Third Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/ui/Navbar";
+import Home from "./pages/Home"; // Import the new Home component
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Other routes can be added here */}
+      </Routes>
+    </>
+  );
+}
+
+export default App;
