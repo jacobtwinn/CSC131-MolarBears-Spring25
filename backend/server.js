86464697acd3da9abc431f6c5a825d4b7426789c
tinowidrin <twidrin@csus.edu>
2025-03-29 14:34:25 -0700

    Added PatientVisitHistory PAge

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
-import path from "path"; // Add this import
-dotenv.config();
-import { connectDB } from "./config/db.js"; // make sure it is db.js and not just db
-connectDB();
-const __dirname = path.resolve(); // Define __dirname for ES module compatibility
+import { connectDB } from "./config/db.js";
 const app = express();
-app.use(express.static(path.join(__dirname, "public"))); // Serve static files
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-app.get("/products",(req, res) => { });
+        app.get("/products",(req, res) => { });
+
+dotenv.config();
+console.log(process.env.MONGO_URI);
 
 app.listen(5000, () => {
-console.log("Server started at http://localhost:5000");
+    console.log("Server started at http://localhost:5000");
+    connectDB();
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/ui/Navbar";
-import Home from "./pages/Home"; // Import the new Home component
+import Navbar from "./ui/Navbar";
+import PatientVisitHistory from "./pages/PatientVisitHistory"; // Import the new Home component
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<PatientVisitHistory />} />
         {/* Other routes can be added here */}
       </Routes>
     </>
