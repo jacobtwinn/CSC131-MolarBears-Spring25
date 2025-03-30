292bf82f7988f0819a15b34de90dc5a94bc97a3f
tinowidrin <twidrin@csus.edu>
2025-03-30 15:07:44 -0700

    Connected database from backend to frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,36 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"
+import { useState, useEffect } from "react"
+import axios from "axios"
 import Navbar from "./ui/Navbar";
-import PatientVisitHistory from "./pages/PatientVisitHistory"; // Import the new Home component
+import PatientVisitHistory from "./pages/PatientVisitHistory" // Import the new Home component
+import "./App.css"
 
 function App() {
+
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    async function grabData() {
+      const response = await axios.get("http://localhost:3000/VisitHistory")
+      console.log(response)
+      if (response.status == 200) {
+        setData(response.data)
+      }
+    }
+
+    grabData()
+  }, [])
+
   return (
     <>
+      {JSON.stringify(data)}
+      {/* The Navbar component will be displayed on all pages */}
       <Navbar />
       <Routes>
         <Route path="/" element={<PatientVisitHistory />} />
-        {/* Other routes can be added here */}
       </Routes>
     </>
-  );
+  )
 }
 
 export default App;
