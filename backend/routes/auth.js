79cbd6e3e518f014dd2af8e9f4d4641fc7be00ab
Jacobtwinn <jtw2106@gmail.com>
2025-03-30 22:59:45 -0700

    Added admin field to user accounts also altered gitignore to filter node_modules files

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,6 +9,7 @@ const router = express.Router();
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
 });
 
 const User = mongoose.model("User", UserSchema);
