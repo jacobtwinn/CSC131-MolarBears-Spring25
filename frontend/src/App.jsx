bd6a8c272c90b86c8407dbcf84015ebecc320402
tinowidrin <twidrin@csus.edu>
2025-04-02 01:41:24 -0700

    Reorganized files and folders to be like main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,10 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import { useState, useEffect } from "react"
 // import { getVisits, getVisit, createVisit, updateVisit, deleteVisit } from "./api"
 // import { PatientVisitHistory } from "./pages/PatientVisitHistory"
-import { Home } from "../pages/Home"
-import { About } from "../pages/About"
-import { Contact } from "../pages/Contact"
-import { Navbar } from "../ui/Navbar"
+import { Home } from "./pages/Home"
+import { About } from "./pages/About"
+import { Contact } from "./pages/Contact"
+import { Navbar } from "./ui/Navbar"
 
 function App() {
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import { Provider } from "../ui/provider"
+import { Provider } from "./ui/provider"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
